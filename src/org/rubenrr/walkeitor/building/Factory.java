9147a01bb2df644bf20ae319a0ba3dfb9d8d211c
Ruben Rubio Rey <tk421.1@gmail.com>
2013-03-25 20:37:10 +1100

    Image resource and missing class

---
+++ src/org/rubenrr/walkeitor/building/Factory.java
@@ -0,0 +1,12 @@
+package org.rubenrr.walkeitor.building;
+
+/**
+ * User: Ruben Rubio Rey
+ * Date: 22/03/13
+ * Time: 7:26 AM
+ *
+ * The factory uses products to create units
+ *
+ */
+public class Factory {
+}
